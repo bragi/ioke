304f4c782f22a412eba1caf104b7425b69dec393
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2009-05-21 15:07:51 +0200

    Changed Extension interface to abstract class

--- src/ikj/main/ioke/lang/extensions/Extension.java
+++ src/ikj/main/ioke/lang/extensions/Extension.java
@@ -10,15 +10,31 @@ import ioke.lang.Runtime;
  * Serves as an entry point for Java-based Ioke extensions.
  *
  * When you create pure Java-based extension for Ioke create a class that
- * implements this interface. Then register your extension within
+ * extends Extension. Then register your extension within
  * Ioke runtime using:
  *
- *   extension(org:example:JavaExtension)
+ *   Extension register("org.example.JavaExtension")
  *
  * from init.ik file in the extension jar file.
  *
  * @author <a href="mailto:bragi@ragnarson.com">Łukasz Piestrzeniewicz</a>
  */
-public interface Extension {
-    public void init(Runtime runtime) throws ControlFlow;
+public abstract class Extension {
+
+    /**
+     * Called when Extension is registered in given +runtime+.
+     *
+     * @param runtime Runtime of the interpreter instance that loaded this Extension.
+     * @throws ioke.lang.exceptions.ControlFlow when something goes wrong.
+     */
+    public void init(Runtime runtime) throws ControlFlow {
+        // Do nothing on purpose.
+    }
+
+    /**
+     * Called when extension is de-registered.
+     */
+    public void shutdown() {
+        // Do nothing on purpose
+    }
 }
--- src/ikj/main/ioke/lang/test/TextExtension.java
+++ src/ikj/main/ioke/lang/test/TextExtension.java
@@ -1,31 +1,32 @@
 /*
- * Adds a single method to Text mimic.
+ * See LICENSE file in distribution for copyright and licensing information.
  */
-
 package ioke.lang.test;
 
 import ioke.lang.IokeObject;
 import ioke.lang.NativeMethod;
 import ioke.lang.Runtime;
 import ioke.lang.exceptions.ControlFlow;
+import ioke.lang.extensions.Extension;
 import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
- *
- * @author bragi
+ * Adds a single test method to Text mimic.
+ * 
+ * @author <a href="mailto:bragi@ragnarson.com">Łukasz Piestrzeniewicz</a>
  */
-public class TextExtension implements ioke.lang.extensions.Extension {
+public class TextExtension extends Extension {
 
     public void init(final Runtime runtime) throws ControlFlow {
         runtime.text.registerMethod(runtime.newNativeMethod("Native extension method always returning true", new NativeMethod.WithNoArguments("extended_via_native_extension?") {
-                @Override
-                public Object activate(IokeObject method, IokeObject context, IokeObject message, Object on) throws ControlFlow {
-                    getArguments().getEvaluatedArguments(context, message, on, new ArrayList<Object>(), new HashMap<String, Object>());
-                    return runtime._true;
-                }
-        }));
-        
-    }
 
+            @Override
+            public Object activate(IokeObject method, IokeObject context, IokeObject message, Object on) throws ControlFlow {
+                getArguments().getEvaluatedArguments(context, message, on, new ArrayList<Object>(), new HashMap<String, Object>());
+                return runtime._true;
+            }
+        }));
+
+    }
 }

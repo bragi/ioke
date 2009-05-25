2d72cbf1efd3779645bacf9db32a6f1f901da5e8
Łukasz Piestrzeniewicz <bragi@ragnarson.com>
2009-05-25 08:56:21 +0200

    Reformatted code, extracted path normalization, added simple debug statements

--- src/ikj/main/ioke/lang/Uses.java
+++ src/ikj/main/ioke/lang/Uses.java
@@ -1,7 +1,6 @@
 /*
  * See LICENSE file in distribution for copyright and licensing information.
  */
-
 package ioke.lang;
 
 import ioke.lang.exceptions.ControlFlow;
@@ -19,16 +18,16 @@ import java.util.Set;
  * @author <a href="mailto:bragi@ragnarson.com">Łukasz Piestrzeniewicz</a>
  */
 public class Uses {
+
     private static final String[] SUFFIXES = {".ik", ".jar"};
     private static final String[] SUFFIXES_WITH_BLANK = {"", ".ik", ".jar"};
-
     private Set<String> loaded = new HashSet<String>();
     private IokeObject loadPath;
     private String currentWorkingDirectory;
     private boolean onWindows;
-    
+
     public boolean isAbsoluteFileName(String name) {
-        if(onWindows) {
+        if (onWindows) {
             return name.length() > 2 && name.charAt(1) == ':' && name.charAt(2) == '\\';
         } else {
             return name.length() > 0 && name.charAt(0) == '/';
@@ -92,22 +91,22 @@ public class Uses {
                         return runtime.nil;
                     }
                 });
-        return !!continueLoadChain[0];
+        return ! !continueLoadChain[0];
     }
 
     private boolean findBuiltin(IokeObject self, IokeObject context, IokeObject message, String name) throws ControlFlow {
         Builtin b = context.runtime.getBuiltin(name);
-        if(b != null) {
-            if(loaded.contains(name)) {
+        if (b != null) {
+            if (loaded.contains(name)) {
                 return false;
             } else {
                 try {
                     b.load(context.runtime, context, message);
                     loaded.add(name);
                     return true;
-                } catch(Throwable e) {
+                } catch (Throwable e) {
                     boolean result = restartLoadingCondition(self, context, message, name, e);
-                    if(!result) {
+                    if (!result) {
                         return false;
                     }
                 }
@@ -116,32 +115,38 @@ public class Uses {
         return true;
     }
 
+    private String normalizedPath(String name, String suffix) {
+        if(name.startsWith(("/"))) {
+            return name + suffix;
+        } else {
+            return "/" + name + suffix;
+        }
+    }
+
     public boolean use(IokeObject self, IokeObject context, IokeObject message, String name) throws ControlFlow {
         final Runtime runtime = context.runtime;
-        if(!findBuiltin(self, context, message, name)) {
+        if (!findBuiltin(self, context, message, name)) {
             return false;
         }
 
-        List<Object> paths = ((IokeList)IokeObject.data(loadPath)).getList();
+        List<Object> paths = ((IokeList) IokeObject.data(loadPath)).getList();
 
         String[] suffixes = (name.endsWith(".ik") || name.endsWith(".jar")) ? SUFFIXES_WITH_BLANK : SUFFIXES;
 
-        // Absolute path
-        for(String suffix : suffixes) {
-            String before = "/";
-            if(name.startsWith("/")) {
-                before = "";
-            }
+        System.out.println("Trying absolute paths");
 
-            InputStream is = IokeSystem.class.getResourceAsStream(before + name + suffix);
+        // Absolute path
+        for (String suffix : suffixes) {
+            System.out.println("Trying absolute paths");
+            InputStream is = IokeSystem.class.getResourceAsStream(normalizedPath(name, suffix));
             try {
                 File f = new File(name + suffix);
 
-                if(f.exists() && f.isFile()) {
-                    if(loaded.contains(f.getCanonicalPath())) {
+                if (f.exists() && f.isFile()) {
+                    if (loaded.contains(f.getCanonicalPath())) {
                         return false;
                     } else {
-                        if(f.getCanonicalPath().endsWith(".jar")) {
+                        if (f.getCanonicalPath().endsWith(".jar")) {
                             context.runtime.classRegistry.getClassLoader().addURL(f.toURI().toURL());
                         } else {
                             context.runtime.evaluateFile(f, message, context);
@@ -152,20 +157,20 @@ public class Uses {
                     }
                 }
 
-                if(null != is) {
-                    if(loaded.contains(name+suffix)) {
+                if (null != is) {
+                    if (loaded.contains(name + suffix)) {
                         return false;
                     } else {
-                        if((name+suffix).endsWith(".jar")) {
+                        if ((name + suffix).endsWith(".jar")) {
                             // load jar here - can't do it correctly at the moment, though.
                         } else {
-                            context.runtime.evaluateStream(name+suffix, new InputStreamReader(is, "UTF-8"), message, context);
+                            context.runtime.evaluateStream(name + suffix, new InputStreamReader(is, "UTF-8"), message, context);
                         }
-                        loaded.add(name+suffix);
+                        loaded.add(name + suffix);
                         return true;
                     }
                 }
-            } catch(Throwable e) {
+            } catch (Throwable e) {
                 boolean result = restartLoadingCondition(self, context, message, name, e);
                 if (!result) {
                     return false;
@@ -176,12 +181,12 @@ public class Uses {
 
 
 
-        for(Object o : paths) {
+        for (Object o : paths) {
             String currentS = Text.getText(o);
 
-            for(String suffix : suffixes) {
+            for (String suffix : suffixes) {
                 String before = "/";
-                if(name.startsWith("/")) {
+                if (name.startsWith("/")) {
                     before = "";
                 }
 
@@ -189,7 +194,7 @@ public class Uses {
                 try {
                     File f;
 
-                    if(isAbsoluteFileName(currentS)) {
+                    if (isAbsoluteFileName(currentS)) {
                         f = new File(currentS, name + suffix);
                     } else {
                         f = new File(new File(currentWorkingDirectory, currentS), name + suffix);
@@ -197,11 +202,11 @@ public class Uses {
 
 //                     System.err.println("trying: " + f);
 
-                    if(f.exists() && f.isFile()) {
-                        if(loaded.contains(f.getCanonicalPath())) {
+                    if (f.exists() && f.isFile()) {
+                        if (loaded.contains(f.getCanonicalPath())) {
                             return false;
                         } else {
-                            if(f.getCanonicalPath().endsWith(".jar")) {
+                            if (f.getCanonicalPath().endsWith(".jar")) {
                                 context.runtime.classRegistry.getClassLoader().addURL(f.toURI().toURL());
                             } else {
                                 context.runtime.evaluateFile(f, message, context);
@@ -212,66 +217,22 @@ public class Uses {
                         }
                     }
 
-                    if(null != is) {
-                        if(loaded.contains(name+suffix)) {
+                    if (null != is) {
+                        if (loaded.contains(name + suffix)) {
                             return false;
                         } else {
-                            if((name+suffix).endsWith(".jar")) {
+                            if ((name + suffix).endsWith(".jar")) {
                                 // load jar here - can't do it correctly at the moment, though.
                             } else {
-                                context.runtime.evaluateStream(name+suffix, new InputStreamReader(is, "UTF-8"), message, context);
+                                context.runtime.evaluateStream(name + suffix, new InputStreamReader(is, "UTF-8"), message, context);
                             }
-                            loaded.add(name+suffix);
+                            loaded.add(name + suffix);
                             return true;
                         }
                     }
-                } catch(Throwable e) {
-                    final IokeObject condition = IokeObject.as(IokeObject.getCellChain(runtime.condition,
-                                                                                       message,
-                                                                                       context,
-                                                                                       "Error",
-                                                                                       "Load"), context).mimic(message, context);
-                    condition.setCell("message", message);
-                    condition.setCell("context", context);
-                    condition.setCell("receiver", self);
-                    condition.setCell("moduleName", runtime.newText(name));
-                    condition.setCell("exceptionMessage", runtime.newText(e.getMessage()));
-                    List<Object> ob = new ArrayList<Object>();
-                    for(StackTraceElement ste : e.getStackTrace()) {
-                        ob.add(runtime.newText(ste.toString()));
-                    }
-
-                    condition.setCell("exceptionStackTrace", runtime.newList(ob));
-
-                    final boolean[] continueLoadChain = new boolean[]{false};
-
-                    runtime.withRestartReturningArguments(new RunnableWithControlFlow() {
-                            public void run() throws ControlFlow {
-                                runtime.errorCondition(condition);
-                            }},
-                        context,
-                        new Restart.ArgumentGivingRestart("continueLoadChain") {
-                            public List<String> getArgumentNames() {
-                                return new ArrayList<String>();
-                            }
-
-                            public IokeObject invoke(IokeObject context, List<Object> arguments) throws ControlFlow {
-                                continueLoadChain[0] = true;
-                                return runtime.nil;
-                            }
-                        },
-                        new Restart.ArgumentGivingRestart("ignoreLoadError") {
-                            public List<String> getArgumentNames() {
-                                return new ArrayList<String>();
-                            }
-
-                            public IokeObject invoke(IokeObject context, List<Object> arguments) throws ControlFlow {
-                                continueLoadChain[0] = false;
-                                return runtime.nil;
-                            }
-                        }
-                        );
-                    if(!continueLoadChain[0]) {
+                } catch (Throwable e) {
+                    boolean result = restartLoadingCondition(self, context, message, name, e);
+                    if (!result) {
                         return false;
                     }
                 }
@@ -279,19 +240,21 @@ public class Uses {
         }
 
         final IokeObject condition = IokeObject.as(IokeObject.getCellChain(runtime.condition,
-                                                                           message,
-                                                                           context,
-                                                                           "Error",
-                                                                           "Load"), context).mimic(message, context);
+                message,
+                context,
+                "Error",
+                "Load"), context).mimic(message, context);
         condition.setCell("message", message);
         condition.setCell("context", context);
         condition.setCell("receiver", self);
         condition.setCell("moduleName", runtime.newText(name));
 
         runtime.withReturningRestart("ignoreLoadError", context, new RunnableWithControlFlow() {
-                public void run() throws ControlFlow {
-                    runtime.errorCondition(condition);
-                }});
+
+            public void run() throws ControlFlow {
+                runtime.errorCondition(condition);
+            }
+        });
         return false;
     }
 }
